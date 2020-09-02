31cda6f76cb458fd86433ff6f209be0fe7490b94
cab40 <69866375+cab40@users.noreply.github.com>
2020-09-01 22:12:00 -0400

    Update show-most-similar-titles.rb

--- show-most-similar-titles.rb
+++ show-most-similar-titles.rb
@@ -7,15 +7,6 @@ def format_similar_message source_book_title, most_similar_title, similarity_sco
     " also liked #{most_similar_title}." +
     " (Score = %0.3f)" % [similarity_score]
 end
-    
-# TODO: Insert your code here to produce the expected output
-# found in expected-output/show-most-similar-titles.txt .
-#
-# To make sure your output is the correct format, use 
-# the above method: format_similar_message
-#
-# To output the books in the correct order, you'll 
-# need process them in order of their book_ids (ascending).
 
 books = Book.get_ids()
 books.sort #now book ids are sorted
